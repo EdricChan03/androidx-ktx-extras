72a191bfbbe40cfc7a8459c719090cf1ba8cb900
Edric <edric.chan.1997@gmail.com>
2023-06-29 00:29:39 +0800

    chore(browser-ktx): remove unused imports for unit test

--- androidx/browser/browser-ktx/src/test/kotlin/io/github/edricchan03/androidx/browser/ktx/enums/ShareStateTest.kt
+++ androidx/browser/browser-ktx/src/test/kotlin/io/github/edricchan03/androidx/browser/ktx/enums/ShareStateTest.kt
@@ -3,15 +3,6 @@ package io.github.edricchan03.androidx.browser.ktx.enums
 import androidx.browser.customtabs.CustomTabsIntent
 import io.github.edricchan03.androidx.browser.ktx.enums.utils.intEnumTests
 import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.matchers.equals.shouldBeEqual
-import io.kotest.matchers.nulls.shouldBeNull
-import io.kotest.matchers.nulls.shouldNotBeNull
-import io.kotest.property.Arb
-import io.kotest.property.Exhaustive
-import io.kotest.property.arbitrary.filterNot
-import io.kotest.property.arbitrary.int
-import io.kotest.property.checkAll
-import io.kotest.property.exhaustive.collection
 
 private val stateIntsMap = mapOf(
     CustomTabsIntent::SHARE_STATE_DEFAULT to ShareState.Default,
@@ -20,8 +11,10 @@ private val stateIntsMap = mapOf(
 )
 
 class ShareStateTest : DescribeSpec({
-    include(intEnumTests(
-        fromValueOrNullFn = ShareState::fromValueOrNull,
-        enumValuesMap = stateIntsMap
-    ))
+    include(
+        intEnumTests(
+            fromValueOrNullFn = ShareState::fromValueOrNull,
+            enumValuesMap = stateIntsMap
+        )
+    )
 })
