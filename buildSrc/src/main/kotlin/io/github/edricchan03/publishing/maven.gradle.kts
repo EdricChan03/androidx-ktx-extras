83f9823d1afd540957ecc2c9629e5f1f38520833
Edric <edric.chan.1997@gmail.com>
2023-06-30 18:12:02 +0800

    ci(buildSrc/maven): add tasks to dump artifacts/repositories

--- buildSrc/src/main/kotlin/io/github/edricchan03/publishing/maven.gradle.kts
+++ buildSrc/src/main/kotlin/io/github/edricchan03/publishing/maven.gradle.kts
@@ -22,11 +22,43 @@ signing {
 tasks {
     // From https://discuss.gradle.org/t/retrieve-gav-of-application-automatically/29889/7
     val dumpPublications by registering {
+        group = PublishingPlugin.PUBLISH_TASK_GROUP
         description = "Dumps the publications for project ${project.name}"
         doLast {
             logger.lifecycle("Publications for project ${project.name}\n${"-".repeat(50)}")
             publishing.publications.withType<MavenPublication> {
                 logger.lifecycle("$name => $groupId:$artifactId:$version")
+            }
+        }
+    }
+
+    val dumpPublicationArtifacts by registering {
+        group = PublishingPlugin.PUBLISH_TASK_GROUP
+        description = "Dumps the publications and their artifacts for project ${project.name}"
+        doLast {
+            logger.let {
+                it.lifecycle("Publication artifacts for project ${project.name}\n${"-".repeat(50)}")
+                publishing.publications.withType<MavenPublication> {
+                    it.lifecycle("$name => $groupId:$artifactId:$version")
+                    it.lifecycle("Artifacts:")
+                    artifacts.all {
+                        it.lifecycle("* $file")
+                    }
+                }
+            }
+        }
+    }
+
+    val dumpMavenRepositories by registering {
+        group = PublishingPlugin.PUBLISH_TASK_GROUP
+        description = "Dumps the publishing Maven repositories for project ${project.name}"
+        doLast {
+            logger.lifecycle(
+                "Publishing Maven repositories for project ${project.name}\n" +
+                    "-".repeat(50)
+            )
+            publishing.repositories.withType<MavenArtifactRepository> {
+                logger.lifecycle("$name => $url")
             }
         }
     }
