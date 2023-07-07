c1fb701def6fd1aa7f4b417b53b6d34f20f9aabd
Edric <edric.chan.1997@gmail.com>
2023-07-07 23:02:34 +0800

    chore: prepare next snapshots

--- androidx/browser/browser-ktx/build.gradle.kts
+++ androidx/browser/browser-ktx/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "io.github.edricchan03.androidx.browser"
-version = "0.0.1"
+version = "0.0.2-SNAPSHOT"
 
 @Suppress("UnstableApiUsage")
 android {
--- androidx/common/common-enums/build.gradle.kts
+++ androidx/common/common-enums/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "io.github.edricchan03.androidx.common.enums"
-version = "0.0.1"
+version = "0.0.2-SNAPSHOT"
 
 @Suppress("UnstableApiUsage")
 android {
