532b4c2e45ae281c273807ad03b56009d6895693
Edric <edric.chan.1997@gmail.com>
2023-08-10 04:47:27 +0800

    fix(browser-ktx): suppress annotation retention lint
    `RequiresOptIn` annotations in Kotlin can be `RUNTIME`, which is the
    default https://kotlinlang.org/docs/opt-in-requirements.html#create-opt-in-requirement-annotations

--- androidx/browser/browser-ktx/src/main/kotlin/io/github/edricchan03/androidx/browser/ktx/annotations/ExperimentalBrowserApi.kt
+++ androidx/browser/browser-ktx/src/main/kotlin/io/github/edricchan03/androidx/browser/ktx/annotations/ExperimentalBrowserApi.kt
@@ -1,10 +1,15 @@
 package io.github.edricchan03.androidx.browser.ktx.annotations
 
+import android.annotation.SuppressLint
+
 /**
  * Denotes APIs that are experimental. Their implementation is likely to be changed,
  * and no backwards compatibility is guaranteed.
  * @since 0.0.1
  */
+// RequiresOptIn annotations can be RUNTIME
+// https://kotlinlang.org/docs/opt-in-requirements.html#create-opt-in-requirement-annotations
+@SuppressLint("ExperimentalAnnotationRetention")
 @RequiresOptIn(
     "This Browser KTX API is experimental and is likely to be changed " +
         "or removed in the future."
