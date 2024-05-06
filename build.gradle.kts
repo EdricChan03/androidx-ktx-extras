1196efb0d664519caafe7994d4ddfc6bf8029ff5
Edric <edric.chan.1997@gmail.com>
2024-05-06 16:26:34 +0800

    chore: bump Dokkatoo to v2.3.1
    * Remove Dokkatoo HTML dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,17 +4,9 @@ plugins {
 
 group = "io.github.edricchan03.androidx"
 
-
 dependencies {
     dokkatoo(projects.androidx.common.commonEnums)
     dokkatoo(projects.androidx.browser.browserKtx)
-
-    // TODO: Remove when https://github.com/adamko-dev/dokkatoo/issues/14 is fixed
-    dokkatooPluginHtml(
-        dokkatoo.versions.jetbrainsDokka.map { dokkaVersion ->
-            "org.jetbrains.dokka:all-modules-page-plugin:$dokkaVersion"
-        }
-    )
 }
 
 dokkatoo {
