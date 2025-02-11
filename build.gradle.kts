ff4a5bfd7aff5064958a05803528018860ce660f
Edric <edric.chan.1997@gmail.com>
2025-02-11 14:04:12 +0800

    docs: fix hyperlink styling for Dokka docs footer message

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,9 @@ dokka {
         customAssets.from(layout.projectDirectory.file("docs/assets/logo-icon.svg"))
         footerMessage.set(
             "&copy; 2023-2025 Edric Chan. androidx-ktx-extras is licensed under the " +
-                "<a href=\"https://github.com/EdricChan03/androidx-ktx-extras/blob/main/LICENSE\">" +
+                "<a href=\"https://github.com/EdricChan03/androidx-ktx-extras/blob/main/LICENSE\" class=\"footer--link footer--link_external\">" +
                 "GNU GPL 3.0</a>. AndroidX is licensed under the " +
-                "<a href=\"https://github.com/androidx/androidx/blob/androidx-main/LICENSE.txt\">" +
+                "<a href=\"https://github.com/androidx/androidx/blob/androidx-main/LICENSE.txt\" class=\"footer--link footer--link_external\">" +
                 "Apache License 2.0</a>."
         )
     }
