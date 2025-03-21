3cc6c7551d53935edbc48ef91c081402beac753f
Edric <edric.chan.1997@gmail.com>
2025-03-21 21:58:05 +0800

    feat(recyclerview-ktx): add method to create `DiffUtil.Callback`
    * Update API dump

--- androidx/recyclerview/recyclerview-ktx/src/main/kotlin/io/github/edricchan03/androidx/recyclerview/ktx/ListAdapterUtils.kt
+++ androidx/recyclerview/recyclerview-ktx/src/main/kotlin/io/github/edricchan03/androidx/recyclerview/ktx/ListAdapterUtils.kt
@@ -52,3 +52,53 @@ public inline fun <T : Any> itemCallback(
     override fun areContentsTheSame(old: T, new: T) = areContentsTheSame(old, new)
     override fun getChangePayload(old: T, new: T) = getChangePayload(old, new)
 }
+
+/**
+ * A Callback class used by [DiffUtil] while calculating the diff between two lists.
+ *
+ * This top-level method allows creating a [DiffUtil.Callback] without requiring an explicit
+ * anonymous object to be created.
+ * @param getOldListSize Returns the size of the old list.
+ * @param getNewListSize Returns the size of the new list.
+ * @param areItemsTheSame Called by the [DiffUtil] to decide whether two object represent the same
+ * Item. For example, if your items have unique ids, this method should check their id equality.
+ * @param areContentsTheSame Called by the [DiffUtil] when it wants to check whether two items
+ * have the same data. [DiffUtil] uses this information to detect if the contents of an item
+ * has changed.
+ *
+ * [DiffUtil] uses this method to check equality instead of [Any.equals] so that you can change
+ * tts behavior depending on your UI. For example, if you are using [DiffUtil] with a
+ * [RecyclerView.Adapter], you should return whether the items' visual representations are the same.
+ * This method is called only if [areItemsTheSame] returns `true` for these items.
+ * @param getChangePayload When [areItemsTheSame] returns `true` for two items and
+ * [areContentsTheSame] returns `false` for them, [DiffUtil] calls this method to get a payload
+ * about the change.
+ *
+ * For example, if you are using [DiffUtil] with [RecyclerView], you can return the particular
+ * field that changed in the item and your [RecyclerView.ItemAnimator] can use that information
+ * to run the correct animation.
+ *
+ * Default implementation returns `null`.
+ * @since 0.2.0
+ */
+@JvmOverloads
+public inline fun callback(
+    crossinline getOldListSize: () -> Int,
+    crossinline getNewListSize: () -> Int,
+    crossinline areItemsTheSame: (oldItemPosition: Int, newItemPosition: Int) -> Boolean,
+    crossinline areContentsTheSame: (oldItemPosition: Int, newItemPosition: Int) -> Boolean,
+    crossinline getChangePayload: (oldItemPosition: Int, newItemPosition: Int) -> Any? = { _, _ -> null }
+): DiffUtil.Callback = object : DiffUtil.Callback() {
+    override fun getOldListSize() = getOldListSize()
+
+    override fun getNewListSize() = getNewListSize()
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+        areItemsTheSame(oldItemPosition, newItemPosition)
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+        areContentsTheSame(oldItemPosition, newItemPosition)
+
+    override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int) =
+        getChangePayload(oldItemPosition, newItemPosition)
+}
