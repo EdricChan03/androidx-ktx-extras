f1b2c8c09e18b54d3b2544266020df969e9e5976
Edric <edric.chan.1997@gmail.com>
2025-08-21 00:53:08 +0800

    build: prep repositories block for KMP support
    See https://youtrack.jetbrains.com/issue/KT-68533

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,11 +6,73 @@ pluginManagement {
     }
 }
 
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    // See https://youtrack.jetbrains.com/issue/KT-68533
+    repositoriesMode = RepositoriesMode.PREFER_SETTINGS
     repositories {
-        google()
-        mavenCentral()
+        mavenCentral {
+            content {
+                excludeGroup("com.yarnpkg")
+                excludeGroup("com.github.webassembly")
+                excludeGroup("org.nodejs")
+                excludeGroup("google.d8")
+            }
+        }
+        google {
+            content {
+                excludeGroup("com.yarnpkg")
+                excludeGroup("com.github.webassembly")
+                excludeGroup("org.nodejs")
+                excludeGroup("google.d8")
+            }
+        }
+        exclusiveContent {
+            forRepository {
+                ivy("https://nodejs.org/dist/") {
+                    name = "Node Distributions at $url"
+                    patternLayout { artifact("v[revision]/[artifact](-v[revision]-[classifier]).[ext]") }
+                    metadataSources { artifact() }
+                    content { includeModule("org.nodejs", "node") }
+                }
+            }
+            filter { includeGroup("org.nodejs") }
+        }
+        exclusiveContent {
+            forRepository {
+                ivy("https://github.com/yarnpkg/yarn/releases/download") {
+                    name = "Yarn Distributions at $url"
+                    patternLayout { artifact("v[revision]/[artifact](-v[revision]).[ext]") }
+                    metadataSources { artifact() }
+                    content { includeModule("com.yarnpkg", "yarn") }
+                }
+            }
+            filter { includeGroup("com.yarnpkg") }
+        }
+        exclusiveContent {
+            forRepository {
+                ivy("https://github.com/WebAssembly/binaryen/releases/download") {
+                    name = "Binaryen Distributions at $url"
+                    patternLayout { artifact("version_[revision]/[module]-version_[revision]-[classifier].[ext]") }
+                    metadataSources { artifact() }
+                    content { includeModule("com.github.webassembly", "binaryen") }
+                }
+            }
+            filter { includeGroup("com.github.webassembly") }
+        }
+        exclusiveContent {
+            forRepository {
+                ivy("https://storage.googleapis.com/chromium-v8/official/canary") {
+                    name = "D8 Distributions at $url"
+                    patternLayout {
+                        artifact("[artifact]-[revision].[ext]")
+                    }
+                    metadataSources { artifact() }
+                    content { includeModule("google.d8", "v8") }
+                }
+            }
+            filter { includeGroup("google.d8") }
+        }
     }
 
     versionCatalogs {
